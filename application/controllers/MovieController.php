1e21835d0b9f2c4b771ab17df5f5e824c5857e6f
rollback-dropdev <felix.wollschlaeger@outlook.de>
2016-11-25 10:37:16 +0100

    ich commite noch schnell konny!

--- application/controllers/MovieController.php
+++ application/controllers/MovieController.php
@@ -3,8 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MovieController extends CI_Controller
 {
-    public function index()
+    public function getMovies()
     {
-        
+        $this->load-model("movie_model");
+        $movies = $this->movie_model->load();
+
+        if (is_numeric($this->input->get("id")))
+        {
+            
+        }
     }
 }
