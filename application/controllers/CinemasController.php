5fc3e4eabdde5bf3e101238273e6ed73a5801ad5
Konstantin Schmidt <assimilationstheorie@gmail.com>
2016-11-28 12:04:05 +0100

    add View

--- application/controllers/CinemasController.php
+++ application/controllers/CinemasController.php
@@ -6,13 +6,15 @@ class CinemasController extends CI_Controller {
      public function index() {
         $this->load->model('cinema_model');
         $getAllCinemas = $this->cinema_model->getAllCinemas();
-        echo json_encode($getAllCinemas);
+        $data['json'] = json_encode($getAllCinemas);
+        $this->load->view('cinema_view', $data);
         #var_dump($getAllCinemas);
     }
 
     public function getCinema($id) {
         $this->load->model('cinema_model');
         $getAllCinemas = $this->cinema_model->getCinema($id);
-        echo json_encode($getAllCinemas);
+        $data['json'] = json_encode($getAllCinemas);
+        $this->load->view('cinema_view', $data);
     }
 }
