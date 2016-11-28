8d31ba97a91e68bbf53cb1f0b8648b41bfae47b8
rollback-dropdev <felix.wollschlaeger@outlook.de>
2016-11-28 12:50:32 +0100

    Filterable GET for movies

--- application/controllers/MovieController.php
+++ application/controllers/MovieController.php
@@ -5,8 +5,33 @@ class MovieController extends CI_Controller
 {
     public function getMovies()
     {
-        $this->load->model("movie_model");
-        $movies = $this->movie_model->loadAll();
+		$cinemaID = null;
+		$minRating = null;
+		$genreID = null;
+		$actorID = null;
+		
+		if ($this->input->get('Cinema_id') !== null && is_numeric($this->input->get('Cinema_id')))
+		{
+			$cinemaID = (int) $this->input->get('Cinema_id');
+		}
+		
+		if ($this->input->get('Min_Rating') !== null && is_numeric($this->input->get('Min_Rating')))
+		{
+			$minRating = (int) $this->input->get('Min_Rating');
+		}
+		
+		if ($this->input->get('Genre_id') !== null && is_numeric($this->input->get('Genre_id')))
+		{
+			$genreID = (int) $this->input->get('Genre_id');
+		}
+		
+		if ($this->input->get('Actor_id') !== null && is_numeric($this->input->get('Actor_id')))
+		{
+			$actorID = (int) $this->input->get('Actor_id');
+		}
+		
+		$this->load->model("movie_model");
+		$movies = $this->movie_model->loadFiltered($cinemaID, $minRating, $genreID, $actorID);
 		
 		$this->load->view('movie_view', $this->prepareDataForView($movies));
     }
--- application/models/Movie_model.php
+++ application/models/Movie_model.php
@@ -47,6 +47,45 @@ class Movie_model extends CI_Model implements JsonSerializable
 		return $movie;
 	}
 	
+	public function loadFiltered($cinemaID, $minRating, $genreID, $actorID)
+	{
+		$builder = $this->db->select('movies.*')->from('movies');
+		
+		if ($cinemaID !== null)
+		{
+			$builder = $builder->join('movie_cinemas', 'movie_cinemas.movie_id = movies.id', 'inner');
+			$builder = $builder->where('movie_cinemas.cinema_id =', $cinemaID);
+		}
+		
+		if ($minRating !== null)
+		{
+			$builder = $builder->join('ratings', 'ratings.movie_id = movies.id', 'inner');
+			$builder = $builder->having('AVG(ratings.rating) >=', $minRating);
+		}
+		
+		if ($genreID !== null)
+		{
+			$builder = $builder->join('movie_genres', 'movie_genres.movie_id = movies.id', 'inner');
+			$builder = $builder->where('movie_genres.genre_id =', $genreID);
+		}
+		
+		if ($actorID !== null)
+		{
+			$builder = $builder->join('movie_actors', 'movie_actors.movie_id = movies.id', 'inner');
+			$builder = $builder->where('movie_actors.actor_id =', $actorID);
+		}
+		
+		$query = $builder->group_by('movies.id')->get();
+		$movies = array();
+
+        foreach ($query->result('Movie_model') as $movie)
+        {
+            $movies[] = $movie;
+        }
+
+        return $movies;
+	}
+	
 	public function jsonSerialize()
 	{
         return (object) get_object_vars($this);
