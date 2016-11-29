b235c6fd139d96f6ba6b01e8660898dc2f50ba8f
Konstantin Schmidt <assimilationstheorie@gmail.com>
2016-11-29 09:48:28 +0100

    add welcome_site

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,8 +4,9 @@
 <meta charset="utf-8">
 <title>PopCornMovie</title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <title>PopCornMovie</title>
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?php echo base_url();?>assets/custom/css/style.css">
 </head>
