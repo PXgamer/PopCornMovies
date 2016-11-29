282c8e58a37479305ce69198f62ed3d3ba06680e
Konstantin Schmidt <assimilationstheorie@gmail.com>
2016-11-29 09:28:59 +0100

    add Jquery, Bootstrap, Custom CSS/JS

---
+++ application/views/templates/footer.php
@@ -0,0 +1,5 @@
+<script src="<?php echo base_url();?>assets/jquery/jquery.js"></script>
+<script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>assets/custom/js/work.js"></script>
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta charset="utf-8">
+<title>PopCornMovie</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>PopCornMovie</title>
+<link rel="stylesheet" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/custom/css/style.css">
+</head>
+<body>
