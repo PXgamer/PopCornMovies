e8336125055c51f0fb382b7cc09cda8d512f9c4e
Konstantin Schmidt <assimilationstheorie@gmail.com>
2016-11-30 15:16:07 +0100

    Revert "fixed autoload"
    This reverts commit 71632cd70997cecd95f927f26b18d2e806bbecb7.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 $autoload['libraries'] = array(
-    'database', 'exceptions/HttpNotFoundException'
+    'database','RestController', 'exceptions/HttpNotFoundException'
 );
 
 /*
