71632cd70997cecd95f927f26b18d2e806bbecb7
start1234567 <t.andershh@web.de>
2016-11-30 13:36:06 +0100

    fixed autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 $autoload['libraries'] = array(
-    'database','RestController', 'exceptions/HttpNotFoundException'
+    'database', 'exceptions/HttpNotFoundException'
 );
 
 /*
